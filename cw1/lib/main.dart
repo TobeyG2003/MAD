3cd2fd80e416f527b81b6122b59aecd1057c61e8
Tobey <TobeyG2003@gmail.com>
2025-09-21 16:18:09 -0400

    remove comments

--- cw1/lib/main.dart
+++ cw1/lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   ThemeMode _themeMode = ThemeMode.system;
-  // This widget is the root of your application.
   void changeTheme(ThemeMode mode) {
     setState(() {
       _themeMode = mode;
@@ -23,21 +22,6 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       darkTheme: ThemeData.dark(),
@@ -54,15 +38,6 @@ class _MyAppState extends State<MyApp> {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title, required this.themeMode, required this.changeTheme});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
   final ThemeMode themeMode;
@@ -88,8 +63,8 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       isDark = true;
     }
      _controller = AnimationController(
-        duration: const Duration(seconds: 1), // Adjust duration as needed
-        vsync: this, // Use TickerProviderStateMixin for vsync
+        duration: const Duration(seconds: 1), 
+        vsync: this,
       );
       _controller.value = 1.0;
       _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
@@ -101,11 +76,6 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     }
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -175,7 +145,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), 
     );
   }
 }
