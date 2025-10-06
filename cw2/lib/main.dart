77acb6e5d11950d65559e80c4e41f292be297999
Tobey <TobeyG2003@gmail.com>
2025-10-06 05:42:14 -0400

    setup buttons and list

--- cw2/lib/main.dart
+++ cw2/lib/main.dart
@@ -29,14 +29,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,18 +40,77 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Expanded(
+              child: Center(
+              child: ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  ListTile(
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('food'),
+                        SizedBox(width: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            /*Navigator.push(
+                              context,
+                              MaterialPageRoute<void>(
+                                builder: (context) => const SecondRoute(),
+                              ),
+                            );*/
+                          },
+                          child: const Text('View Recipe'),
+                        ),
+                      ],
+                    ),
+                  ),
+                  ListTile(
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('food'),
+                        SizedBox(width: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            /*Navigator.push(
+                              context,
+                              MaterialPageRoute<void>(
+                                builder: (context) => const SecondRoute(),
+                              ),
+                            );*/
+                          },
+                          child: const Text('View Recipe'),
+                        ),
+                      ],
+                    ),
+                  ),
+                  ListTile(
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('food'),
+                        SizedBox(width: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            /*Navigator.push(
+                              context,
+                              MaterialPageRoute<void>(
+                                builder: (context) => const SecondRoute(),
+                              ),
+                            );*/
+                          },
+                          child: const Text('View Recipe'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
